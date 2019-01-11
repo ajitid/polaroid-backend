4fd1f7f1bee9e382a909a118e935bcf8719ddf86
Ajit Singh <ajit.singh2905@gmail.com>
2019-01-12 04:38:29 +0530

    changes

--- src/authentication/urls.py
+++ src/authentication/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("login", views.LoginView.as_view(), name="token_login"),
     path("logout", knox_views.LogoutView.as_view(), name="token_logout"),
     path("logoutall", knox_views.LogoutAllView.as_view(), name="token_logoutall"),
+    path("user", views.user_info, name="user_info"),
 ]
--- src/authentication/views.py
+++ src/authentication/views.py
@@ -1,7 +1,16 @@
-from knox.views import LoginView as KnoxLoginView
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import BasicAuthentication
+from rest_framework.response import Response
+from knox.views import LoginView as KnoxLoginView
 
 
 class LoginView(KnoxLoginView):
     authentication_classes = [BasicAuthentication]
     permission_classes = []
+
+
+@api_view()
+@permission_classes([IsAuthenticated])
+def user_info(request):
+    return Response({"username": request.user.username})
